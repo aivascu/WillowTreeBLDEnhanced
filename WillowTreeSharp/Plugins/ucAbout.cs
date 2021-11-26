b2c851962d59ad2b2aeaa86d8651e982c432285f
Andrei Ivascu <7030530+aivascu@users.noreply.github.com>
2021-11-26 03:51:05 +0200

    Implemented presenter for about view

--- WillowTreeSharp/Plugins/ucAbout.cs
+++ WillowTreeSharp/Plugins/ucAbout.cs
@@ -1,58 +1,28 @@
 ﻿using System;
-using System.Diagnostics;
-using System.Net;
 using System.Windows.Forms;
 
 namespace WillowTree.Plugins
 {
     public partial class UcAbout : UserControl, IPlugin
     {
-        private string downloadUrlFromServer;
-        private string versionFromServer;
-
         public UcAbout()
         {
             this.InitializeComponent();
         }
 
-        /// <summary>
-        /// Recovers the latest version from the sourceforge server.
-        /// </summary>
-        public void CheckVersion(object state)
-        {
-            try
-            {
-                using (var webClient = new WebClient())
-                {
-                    var versionTextFromServer =
-                        webClient.DownloadString("http://willowtree.sourceforge.net/version.txt");
-                    var remoteVersionInfo = versionTextFromServer.Replace("\r\n", "\n").Split('\n');
-                    if (remoteVersionInfo.Length <= 1 && remoteVersionInfo.Length > 3)
-                    {
-                        return;
-                    }
-
-                    this.versionFromServer = remoteVersionInfo[0];
-                    this.downloadUrlFromServer = remoteVersionInfo[1];
-                }
-            }
-            catch
-            {
-                // ignored
-            }
-        }
+        public event EventHandler Tick;
+        public event EventHandler UpdateRequest;
+        public event EventHandler ViewSelect;
 
         public void InitializePlugin(PluginComponentManager pm)
         {
             var events = new PluginEvents
             {
-                PluginSelected = OnPluginSelected,
-                PluginUnselected = OnPluginUnselected
+                PluginSelected = this.OnPluginSelected,
+                PluginUnselected = this.OnPluginUnselected
             };
             pm.RegisterPlugin(this, events);
 
-            // Only check for new version if it's not a debug build.
-            //ThreadPool.QueueUserWorkItem(CheckVersion);
             this.UpdateButton.Hide();
         }
 
@@ -60,28 +30,9 @@ namespace WillowTree.Plugins
         {
         }
 
-        private static string GetVersion()
-        {
-            return "2.2.1";
-        }
-
-        private void CheckVerPopup()
-        {
-            if (this.versionFromServer == GetVersion() || string.IsNullOrEmpty(this.versionFromServer))
-            {
-                return;
-            }
-
-            this.UpdateButton.Text = $"Version {this.versionFromServer} is now available! Click here to download.";
-            this.UpdateButton.Show();
-        }
-
         private void OnPluginSelected(object sender, PluginEventArgs e)
         {
-            if (this.versionFromServer == null)
-            {
-                this.timer1.Enabled = true;
-            }
+            this.ViewSelect?.Invoke(this, e);
         }
 
         private void OnPluginUnselected(object sender, PluginEventArgs e)
@@ -91,18 +42,32 @@ namespace WillowTree.Plugins
 
         private void OnTimerTick(object sender, EventArgs e)
         {
-            if (this.versionFromServer == null)
-            {
-                return;
-            }
-
-            this.timer1.Enabled = false;
-            this.CheckVerPopup();
+            this.Tick?.Invoke(this, e);
         }
 
         private void UpdateButton_Click(object sender, EventArgs e)
         {
-            Process.Start($"http://{this.downloadUrlFromServer}");
+            this.UpdateRequest?.Invoke(this, e);
+        }
+
+        public void SetUpdateButtonText(string text)
+        {
+            this.UpdateButton.Text = text;
+        }
+
+        public void ShowUpdateButton()
+        {
+            this.UpdateButton.Show();
+        }
+
+        public void DisableTimer()
+        {
+            this.timer1.Enabled = false;
+        }
+
+        public void EnableTimer()
+        {
+            this.timer1.Enabled = false;
         }
     }
 }
---
+++ WillowTreeSharp/Presenters/AboutViewPresenter.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Diagnostics;
+using System.Net;
+using WillowTree.Plugins;
+
+namespace WillowTree.Presenters
+{
+    public class AboutViewPresenter
+    {
+        private string downloadUrlFromServer;
+        private string versionFromServer;
+
+        private readonly UcAbout aboutView;
+
+        public AboutViewPresenter(UcAbout aboutView)
+        {
+            this.aboutView = aboutView;
+            this.aboutView.Tick += this.OnTick;
+            this.aboutView.UpdateRequest += this.OnUpdateRequested;
+            this.aboutView.ViewSelect += this.OnViewSelected;
+        }
+
+        private void OnViewSelected(object sender, EventArgs e)
+        {
+            if (this.versionFromServer == null)
+            {
+                this.aboutView.EnableTimer();
+            }
+        }
+
+        private void OnUpdateRequested(object sender, EventArgs e)
+        {
+            Process.Start($"http://{this.downloadUrlFromServer}");
+        }
+
+        private void OnTick(object sender, EventArgs e)
+        {
+            if (this.versionFromServer == null)
+            {
+                return;
+            }
+
+            this.aboutView.DisableTimer();
+            this.CheckVerPopup();
+        }
+
+        /// <summary>
+        /// Recovers the latest version from the sourceforge server.
+        /// </summary>
+        public void CheckVersion(object state)
+        {
+            try
+            {
+                using (var webClient = new WebClient())
+                {
+                    var versionTextFromServer =
+                        webClient.DownloadString("https://willowtree.sourceforge.net/version.txt");
+                    var remoteVersionInfo = versionTextFromServer.Replace("\r\n", "\n").Split('\n');
+                    if (remoteVersionInfo.Length <= 1 && remoteVersionInfo.Length > 3)
+                    {
+                        return;
+                    }
+
+                    this.versionFromServer = remoteVersionInfo[0];
+                    this.downloadUrlFromServer = remoteVersionInfo[1];
+                }
+            }
+            catch
+            {
+                // ignored
+            }
+        }
+
+        
+        private static string GetVersion()
+        {
+            return "2.2.1";
+        }
+        
+        private void CheckVerPopup()
+        {
+            if (this.versionFromServer == GetVersion() || string.IsNullOrEmpty(this.versionFromServer))
+            {
+                return;
+            }
+
+            this.aboutView.SetUpdateButtonText(
+                $"Version {this.versionFromServer} is now available! Click here to download.");
+            this.aboutView.ShowUpdateButton();
+        }
+    }
+}
